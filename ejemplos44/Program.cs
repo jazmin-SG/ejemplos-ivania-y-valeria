00baec2cf81bf74204478a3842129c48ef524755
ivani <ivani@ARES>
2026-03-11 10:56:09 -0600

    Agregar archivos de proyecto.

---
+++ ejemplos44/Program.cs
@@ -0,0 +1,71 @@
+﻿class ejemploAdmid
+    {
+    public static void Main(String[] args)
+    {
+
+        string nombre;
+        int op;
+        Console.WriteLine(" 1 admin");
+        Console.WriteLine(" 1 estudiante");
+        op=int.Parse(Console.ReadLine());
+        switch (op)
+        {
+            case 1:
+                Console.WriteLine("Ingrese su usuario");
+                string usuario = Console.ReadLine();
+                Console.WriteLine(" Ingrese su contraseña");
+                string pasw = (Console.ReadLine());
+                if (usuario == "admin")
+                {
+                    if (pasw == "123")
+                    {
+                        Console.WriteLine("Acceso corecto");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" contraseña incorecta");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("contraseña incorecta");
+                }
+                Console.WriteLine(" 1 nota");
+                Console.WriteLine(" 2 lista de estudiante");
+                op= int.Parse(Console.ReadLine());
+                switch (op)
+                {
+
+                }
+
+
+                break;
+            case 2:
+                Console.WriteLine(" ingrese su usuario");
+                string alumno = Console.ReadLine();
+                Console.WriteLine("Ingrese la contraseña");
+                string cont = Console.ReadLine();
+                if (alumno == "alumno")
+                {
+                    if (cont =="1234")
+                    {
+                        Console.WriteLine("Acceso corecto");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contraseña incorrecta");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Usuario incorrecto");
+                }
+       
+
+                break;
+        }
+    
+    }
+
+}
